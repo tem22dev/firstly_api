8187dbdd6b1f0893b8a48af7c5820bae6743125a
Trung Em ^_^ <tem2k2.dev@gmail.com>
2024-04-17 10:49:37 +0700

    More Routes

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
+app.get('/user', (req, res) => {
+    res.json({id: 1, name: 'Trung Em', email: 'bakhia123@gmail.com'});
+});
+
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    res.send(`<h1>Hello world</h1>`)
 });
 
 app.listen(port, () => {
